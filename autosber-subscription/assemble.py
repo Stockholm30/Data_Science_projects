b8eb085d39251c7a6e544fa108af429ea4d6c52b
Sonya Green <94986520+Stockholm30@users.noreply.github.com>
2024-04-02 11:32:43 +0300

    Update assemble.py

--- autosber-subscription/assemble.py
+++ autosber-subscription/assemble.py
@@ -1,13 +1,20 @@
 import os
 import settings
 import pandas as pd
-import sys
-import requests
-!pip install -U -q PyDrive
 
-
-data_hits=
-data_sessions=
 
 HEADERS={
+    "ga_hits": [
+        'session_id', 'hit_date', 'hit_time', 'hit_number', 'hit_type',
+       'hit_referer', 'hit_page_path', 'event_category', 'event_action',
+       'event_label', 'event_value'
+    ],
+  "ga_sessions": [
+        'session_id', 'client_id', 'visit_date', 'visit_time', 'visit_number',
+       'utm_source', 'utm_medium', 'utm_campaign', 'utm_adcontent',
+       'utm_keyword', 'device_category', 'device_os', 'device_brand',
+       'device_model', 'device_screen_resolution', 'device_browser',
+       'geo_country', 'geo_city'
+    ]
+}
   
